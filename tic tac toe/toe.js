5ad5ead0353acaab1a7c585b797e80c20c30829d
MrArturtleNSCDS <agarcia@nscds.org>
2019-12-16 19:34:13 +0000

    Comments in JS and HTML

--- tic tac toe/toe.js
+++ tic tac toe/toe.js
@@ -1,4 +1,4 @@
 $(".box").click(function() {
-    $(".box").css("background-color", "white");
-    $(this).css("background-color", "red");
+    $(".box").css("background-color", "white"); //If you do this with every click, any box that has changed color will reset to white
+    $(this).css("background-color", "red");  //You only need this one. 
 });
