4c9d29e971681c04b1c56bbf9c44b7784244f0d0
gpedroza21 <gpedroza21@nscds.org>
2019-12-18 19:15:20 +0000

    made box change different color each click

--- tic tac toe/toe.js
+++ tic tac toe/toe.js
@@ -4,13 +4,20 @@
 
 /* Good progress knowing you need an if statement. Just make sure that your if
 is evealuation the right condition.*/
+  var clicks = 0
 
-$('.box').click(function() {
-  var clicks = $(this).css("background-color", "red"); // what does clicks equal after this line?
-  if (clicks) { // clicks should be a number that can be determined odd or even, the line above does not assign a number to clicks
-     // odd clicks
-  } else {
-     // even clicks
-  }
-  $(this).css("background-color","white", !clicks);
-});
+
+$(".box").one("click",function() {
+    var color;
+    if (clicks%2==1) { // clicks should be a number that can be determined odd or even, the line above does not assign a number to clicks
+        color="red";
+    }
+    else{
+        color="white";
+    }
+    $(this).css("background-color",color);
+    clicks++;
+
+}
+
+);
