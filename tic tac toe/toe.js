8306bad4a4b524fab0e89996b70c25484f325fc8
MrArturtleNSCDS <agarcia@nscds.org>
2019-12-18 17:22:36 +0000

    Added comments

--- tic tac toe/toe.js
+++ tic tac toe/toe.js
@@ -2,9 +2,12 @@
     $(this).css("background-color", "red");
 });*/
 
+/* Good progress knowing you need an if statement. Just make sure that your if
+is evealuation the right condition.*/
+
 $('.box').click(function() {
-  var clicks = $(this).css("background-color", "red");
-  if (clicks) { 
+  var clicks = $(this).css("background-color", "red"); // what does clicks equal after this line?
+  if (clicks) { // clicks should be a number that can be determined odd or even, the line above does not assign a number to clicks
      // odd clicks
   } else {
      // even clicks
