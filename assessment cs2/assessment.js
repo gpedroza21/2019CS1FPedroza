976254c0367fa0fc99e51257f09dc3a0731a59c7
gpedroza21 <gpedroza21@nscds.org>
2020-03-09 18:17:42 +0000

    assessment

---
+++ assessment cs2/assessment.js
@@ -0,0 +1,19 @@
+$('.boxes').click(
+        function(){
+            var color = $(this).css("background-color")
+            $('#longBox').css("background-color",color);
+
+            changeElements.each(
+                function(){
+                var change = $('#darkRed').css();
+                if( click == change){
+                $('#newNumber').text("3");
+                }
+            
+            
+           
+        }
+        
+)
+    }
+)
